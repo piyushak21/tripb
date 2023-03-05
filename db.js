c07134d4b670a4e1b3fbb684b585d484315217b6
piyushak21 <106967130+piyushak21@users.noreply.github.com>
2023-03-05 16:42:41 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nodeapi",
+   host: "stknportal.cuce9do7mxez.ap-south-1.rds.amazonaws.com",
+  user: "starkennadmin",
+  password: "4x7SbTLSLH5NC9Yn43E0",
+  database: "stakennportal",
+  port: 3306,
+  connectionLimit: 10,
 });
